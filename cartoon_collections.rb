22d6ce2854ced48eac429f2548b6eba51e514c63
Anthony Alcorn <xmastony@gmail.com>
2020-08-18 06:24:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
   string.upcase
   planeteer_calls.map |string|
   "#{string}!"
-  planeteer_calls
+  p planeteer_calls
 end
 
 def long_planeteer_calls(planeteer_calls)
