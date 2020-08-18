4460401c2c2452ee46757f362451984fd5bb92ee
Anthony Alcorn <xmastony@gmail.com>
2020-08-18 07:24:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,6 @@ def find_valid_calls(planeteer_calls)
   planeteer_calls.find do |element|
     element == "Earth!" || element == "Wind!"|| element == "Fire!" || element == "Water!" || element == "Heart!"|
 end
-end
 
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
