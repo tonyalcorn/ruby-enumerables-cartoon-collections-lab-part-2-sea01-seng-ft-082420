e4361667ac4e9abd2f8cd5658bd128cf8b5deb2a
Anthony Alcorn <xmastony@gmail.com>
2020-08-18 07:08:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
     element = valid_calls
+  else nil
   end
 end
 
